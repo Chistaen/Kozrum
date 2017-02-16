dfc9e8ee5a6674c188d62731dffc853350c5c1e0
Robert Monden <cj_robert@outlook.com>
2017-02-16 12:57:02 +0100

    Added grammar class

---
+++ src/Grammar.cpp
@@ -0,0 +1,15 @@
+/**
+ * KOZRUM - HTML content generator
+ *
+ * @version     1.0.0
+ * @author      Robert Monden
+ * @copyright   2017, Robert Monden
+ * @license     Apache 2.0
+ */
+
+#include "Grammar.h"
+
+Grammar::Grammar()
+{
+
+}
---
+++ src/Grammar.h
@@ -0,0 +1,21 @@
+/**
+ * KOZRUM - HTML content generator
+ *
+ * @version     1.0.0
+ * @author      Robert Monden
+ * @copyright   2017, Robert Monden
+ * @license     Apache 2.0
+ */
+
+#ifndef KOZRUM_GRAMMAR_H
+#define KOZRUM_GRAMMAR_H
+
+
+class Grammar
+{
+public:
+    Grammar();
+};
+
+
+#endif //KOZRUM_GRAMMAR_H
